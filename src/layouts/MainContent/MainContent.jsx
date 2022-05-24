a11557017653a38414e21ed05a03249c89edfb98
Cao Yue <571119725@qq.com>
2022-05-25 00:39:50 +0800

    修理了动画的bug

--- src/layouts/MainContent/MainContent.jsx
+++ src/layouts/MainContent/MainContent.jsx
@@ -5,14 +5,14 @@ import {Route, Routes, useLocation} from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 function MainContent () {
   const location = useLocation();
-  console.log(styles.fade);
+  console.log(location);
   return (
     <div className={styles.main_content}>
       <TransitionGroup className={styles.router_wrapper}>
         <CSSTransition 
           timeout={1000} 
           classNames={'fade'} key={location.pathname} unmountOnExit={true} appear={true}>
-          <Routes>
+          <Routes location={location}>
             <Route path='/' element={<ChatPage />}/>
             <Route path='/download' element={<DownloadPage />}/>
             <Route path='/setting' element={<SettingPage />}/>
