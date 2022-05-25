825b624fbe29bba0aef7380539fe8c47cdc3514b
Cao Yue <571119725@qq.com>
2022-05-25 11:23:03 +0800

    bug

--- src/components/ShowDrawerButton/ShowDrawerButton.jsx
+++ src/components/ShowDrawerButton/ShowDrawerButton.jsx
@@ -1,8 +1,14 @@
 import styles from './showDrawerButton.module.css';
 function ShowDrawerButton () {
+  let showMask = false;
+  const onChangeMaskShow = () => {
+    showMask = !showMask;
+    console.log(showMask);
+  };
   return (
     <div className={styles.show_drawer_button}>
-      <div className='iconfont'>&#xe6a1;</div>
+      <div className='iconfont' onClick={onChangeMaskShow}>&#xe6a1;</div>
+      <div className={(showMask ? styles.mask_show:'')}></div>
     </div>
   )
 };
