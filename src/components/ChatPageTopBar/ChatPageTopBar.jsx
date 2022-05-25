7555cb994c02dbd6dda8f4ea0807a8b0a9f3f61b
caozheng <571119725@qq.com>
2022-05-25 10:37:23 +0800

    bug

--- src/components/ChatPageTopBar/ChatPageTopBar.jsx
+++ src/components/ChatPageTopBar/ChatPageTopBar.jsx
@@ -1,18 +1,17 @@
 import styles from './chatPageTopBar.module.css';
+import ShowDrawerButton from '../ShowDrawerButton/ShowDrawerButton';
 function ChatPageTopBar () {
-    const onFunctButtonClick = () => {
-        console.log(1111);
-    };
     return (
         <div className={styles.chat_page_top_bar}>
             <div className={styles.chat_info}>
+                <ShowDrawerButton />
                 <div className={styles.head_portrait}></div>
                 <div className={styles.name_and_signature}>
                     <div className={styles.name}>数字人组</div>
                     <div className={styles.personal_signature}>旨在制作一款聊天机器人</div>
                 </div>
             </div>
-            <div className={styles.chat_funct_choose} onClick={onFunctButtonClick}>
+            <div className={styles.chat_funct_choose}>
                 <div className='iconfont'>&#xe611;</div>
             </div>    
         </div>
--- src/components/ShowDrawerButton/ShowDrawerButton.jsx
+++ src/components/ShowDrawerButton/ShowDrawerButton.jsx
@@ -1,6 +1,9 @@
+import styles from './showDrawerButton.module.css';
 function ShowDrawerButton () {
   return (
-    <div></div>
+    <div className={styles.show_drawer_button}>
+      <div className='iconfont'>&#xe6a1;</div>
+    </div>
   )
 };
 export default ShowDrawerButton;
