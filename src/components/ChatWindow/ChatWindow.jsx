53f82e1d46c578d83501b56bf5e99961c9f70570
caozheng <571119725@qq.com>
2022-06-04 12:45:30 +0800

    finish

--- src/components/ChatWindow/ChatWindow.jsx
+++ src/components/ChatWindow/ChatWindow.jsx
@@ -3,8 +3,8 @@ import './animation.css';
 import React, { useImperativeHandle, useRef } from 'react';
 import QuestionDialog from '@/components/QuestionDialog/QuestionDialog';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
-// import DateColumn from '@/components/DateColumn/DateColumn';
-// import CheckHistoryDialog from '@/components/CheckHistoryDialog/checkHistoryDialog';
+import DateColumn from '@/components/DateColumn/DateColumn';
+import CheckHistoryDialog from '@/components/CheckHistoryDialog/checkHistoryDialog';
 import ResponseDialog from '@/components/ResponseDialog/ResponseDialog';
 function ChatWindow (props) {
   useImperativeHandle(props.onRef, () => {
@@ -22,31 +22,45 @@ function ChatWindow (props) {
   }
   const Dialogs = props.converInfor.map( (element,id) => {
     let temp;
-    if (element.selfuser) {
+    if(element.type === -1){
       temp = 
-        <CSSTransition 
-          timeout={700} 
-          classNames={'appearLTR'} 
-          unmountOnExit={true} 
-          appear={true}
-          key={id}>
-          <QuestionDialog content={element.content} time={element.reply_time.slice(11,16)}/>
-        </CSSTransition>;
+      <CSSTransition 
+        timeout={700} 
+        classNames={'appearTTB'} 
+        unmountOnExit={true} 
+        appear={true}
+        key={id}>
+        <DateColumn date={element.date}/>
+      </CSSTransition>;
     }else {
-      temp = 
-        <CSSTransition 
-          timeout={700} 
-          classNames={'appearRTL'} 
-          unmountOnExit={true} 
-          appear={true}
-          key={id}>
-          <ResponseDialog 
-            id={element.msgid}
-            content={element.content} 
-            time={element.reply_time.slice(11,16)}
-            evaluation={element.evaluation}
-            onChooseLevel={props.onChooseLevel}/>
-        </CSSTransition>;
+      if (element.selfuser) {
+        temp = 
+          <CSSTransition 
+            timeout={700} 
+            classNames={'appearLTR'} 
+            unmountOnExit={true} 
+            appear={true}
+            key={id}>
+            <QuestionDialog 
+              content={element.content} 
+              time={element.reply_time.slice(11,16)}/>
+          </CSSTransition>;
+      }else {
+        temp = 
+          <CSSTransition 
+            timeout={700} 
+            classNames={'appearRTL'} 
+            unmountOnExit={true} 
+            appear={true}
+            key={id}>
+            <ResponseDialog 
+              id={element.msgid}
+              content={element.content} 
+              time={element.reply_time.slice(11,16)}
+              evaluation={element.evaluation}
+              onChooseLevel={props.onChooseLevel}/>
+          </CSSTransition>;
+      }
     }
     return (
       temp
@@ -54,11 +68,10 @@ function ChatWindow (props) {
   });
   return (
     <div className={styles.chat_window}  ref={messageEndRef}>
-      {/* <CheckHistoryDialog /> */}
+      <CheckHistoryDialog checkHistoryDialog={props.checkHistoryDialog}/>
       <TransitionGroup className={styles.router_wrapper}>
         {Dialogs}
       </TransitionGroup>
-      {/* <DateColumn date='2020-10-13'/> */}
     </div>
   )
 }
--- src/views/ChatPage/ChatPage.jsx
+++ src/views/ChatPage/ChatPage.jsx
@@ -6,6 +6,8 @@ import {getHistoryMsgSingleDay, evaluateMessage} from '@/apis';
 import moment from 'moment';
 function ChatPage () {
   const [dialogInfor,setDialogInfor] = useState([]);
+  const [currentDay, setCurrentDay] = useState('');
+  const [inputNewMessage, setInputNewMessage] = useState(false);
   const childRef = React.createRef();
   useEffect(
     () => {
@@ -16,22 +18,50 @@ function ChatPage () {
       };
       getHistoryMsgSingleDay(infor).then(
         res => {
-          const temp = [];
-          temp.push({date: res.day, type: -1});
-          temp.push(...res.content);
-          console.log(temp);
+          if(res) {
+            const temp = [];
+            temp.push({date: res.day, type: -1});
+            temp.push(...res.content);
+            setDialogInfor([...temp]);
+            setCurrentDay(res.day);
+          }
         }
       )
-    }
+    },[]
   )
   useEffect(
     () => {
-      childRef.current.scrollToBottom();
+      if(inputNewMessage){
+        childRef.current.scrollToBottom();
+        setInputNewMessage(false);
+      }
     },[dialogInfor,childRef]
   );
   const showContentInfor = (infor) => {
     dialogInfor.push(infor);
     setDialogInfor([...dialogInfor]);
+    setInputNewMessage(true);
+  };
+  const checkHistoryDialog = () => {
+    const infor = {
+      userid: sessionStorage.getItem('userId'),
+      current_day: currentDay,
+      is_today: 0
+    };
+    getHistoryMsgSingleDay(infor).then(
+      res => {
+        if(res) {
+          const newData = [];
+          newData.push({date: res.day, type: -1});
+          newData.push(...res.content);
+          const temp = dialogInfor;
+          temp.unshift(...newData);
+          console.log(temp);
+          setDialogInfor([...temp])
+          setCurrentDay(res.day);
+        }
+      }
+    )
   };
   const chooseLevel = (id, level) => {
     let temp = [...dialogInfor];
@@ -46,10 +76,10 @@ function ChatPage () {
       evaluate: level
     };
     evaluateMessage(infor);
-  }
+  };
   return (
     <div className={styles.chat_page}>
-      <ChatWindow converInfor={dialogInfor} onRef={childRef} onChooseLevel={(id,level) => chooseLevel(id,level)}/>
+      <ChatWindow converInfor={dialogInfor} onRef={childRef} onChooseLevel={(id,level) => chooseLevel(id,level)} checkHistoryDialog={checkHistoryDialog}/>
       <InputArea getInputContentInfor={showContentInfor}/>
     </div>
   )
